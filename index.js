2e37d00297c91ce1896384d402f1c3cd3602d940
Priyanka B <pbhogekar09@gmail.com>
2025-08-07 18:31:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+const {connectToMongoDB} = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const { checkForAuthentication,restrictTo } = require("./middlewares/auth");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(()=>console.log("MongoDB Connected"));
+
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+app.use(cookieParser());
+app.use(checkForAuthentication); // Middleware to check for authentication
+
+app.get("/test",async(req,res)=>{
+    const allUrls= await URL.find({});
+    return res.render("home",{urls:allUrls});
+});
+
+app.use("/url", restrictTo(["NORMAL"]), urlRoute);
+app.use("/user",userRoute);
+app.use("/", staticRoute);
+
+app.get('/:shortId',async (req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp : Date.now(),
+                },
+            },
+        }
+    );
+    if (!entry) return res.status(404).send("Short URL not found");
+    res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server started at Port : ${PORT}`));
